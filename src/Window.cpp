7965637b173068b06d4f226d15d5793908f424fb
Aleksandr <obscure812@gmail.com>
2021-04-27 05:51:30 +0300

    Scroll fix

--- src/Window.cpp
+++ src/Window.cpp
@@ -1,17 +1,18 @@
 #pragma once
 #include <Window.h>
 
+#include <QScrollBar>
+
 
 Window::Window()
 	: m_Layout(this), m_View(&m_Scene)
 {
-	setFixedSize(1280, 720);
-
 	m_Layout.addWidget(&m_View);
 	m_View.setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 	m_View.setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 	m_View.setViewportUpdateMode(QGraphicsView::BoundingRectViewportUpdate);
+	m_View.horizontalScrollBar()->blockSignals(true);
+	m_View.verticalScrollBar()->blockSignals(true);
 	m_Scene.setItemIndexMethod(QGraphicsScene::ItemIndexMethod::BspTreeIndex);
 	m_Scene.setSceneRect(0, 0, 2000, 2000);
-
 }
