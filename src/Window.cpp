4f93edb771da0add0a457dcc58cbbdefd6b6d0aa
Aleksandr <obscure812@gmail.com>
2021-05-04 21:26:08 +0300

    minor fix

--- src/Window.cpp
+++ src/Window.cpp
@@ -7,10 +7,10 @@ Window::Window()
 	: m_Layout(this), m_View(&m_Scene)
 {
 	m_Layout.addWidget(&m_View);
+
 	m_View.setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 	m_View.setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 	m_View.horizontalScrollBar()->blockSignals(true);
 	m_View.verticalScrollBar()->blockSignals(true);
-	m_Scene.setItemIndexMethod(QGraphicsScene::ItemIndexMethod::BspTreeIndex);
 	m_Scene.setSceneRect(0, 0, 2000, 2000);
 }
